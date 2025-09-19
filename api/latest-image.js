499b9ef1d081b756c069553ccf7c9f9ddc6123d4
np9258175-ux <np9258175@gmail.com>
2025-09-19 20:55:35 +0530

    Update latest-image.js

--- api/latest-image.js
+++ api/latest-image.js
@@ -1,5 +1,10 @@
-import { latestImage } from "./mh-webhook";
+import { latestImages } from "./mh-webhook";
 
 export default function handler(req, res) {
-  res.status(200).json({ url: latestImage });
+  const prompt = req.query.prompt;
+  if (prompt && latestImages[prompt]) {
+    res.status(200).json({ url: latestImages[prompt] });
+  } else {
+    res.status(404).json({ url: null });
+  }
 }
